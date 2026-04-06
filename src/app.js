dd5f75a128666e454903e117b31e3d1b79bf814e
Sharafath Ali <sharafathaliofficial@gmail.com>
2026-04-07 00:10:24 +0530

    feat: initialize express application with cors, cookie-parser, and security middleware

--- src/app.js
+++ src/app.js
@@ -2,9 +2,13 @@ import express from 'express';
 import logger from '#config/logger.js';
 import helmet from 'helmet';
 import morgan from 'morgan';
+import cors from 'cors';
+import cookieParser from 'cookie-parser';
 
 export const app = express();
 
+app.use(cors());
+app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
