629f5f67e8554bf91727167f6821cd7fca14a914
Sharafath Ali <sharafathaliofficial@gmail.com>
2026-04-07 00:06:54 +0530

    feat: initialize app entry point with express middleware and configure path aliases

--- src/app.js
+++ src/app.js
@@ -1,9 +1,15 @@
 import express from 'express';
-import logger from './config/logger.js';
+import logger from '#config/logger.js';
+import helmet from 'helmet';
+import morgan from 'morgan';
 
 export const app = express();
 
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(helmet());
+
+app.use(morgan('combined', { stream: logger.stream }));
 
 app.get('/', (req, res) => {
   logger.info('NexDeal API is running!');
