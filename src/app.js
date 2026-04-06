139e980343a51b811b73722def708905dd28b514
Sharafath Ali <sharafathaliofficial@gmail.com>
2026-04-06 22:51:27 +0530

    feat: initialize Express application and configure initial logging service

--- src/app.js
+++ src/app.js
@@ -1,9 +1,11 @@
 import express from 'express';
+import logger from './config/logger.js';
 
 export const app = express();
 
 app.use(express.json());
 
 app.get('/', (req, res) => {
+  logger.info('NexDeal API is running!');
   res.send('NexDeal API is running!');
 });
