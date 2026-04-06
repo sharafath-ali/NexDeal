ab4a4d155587b19da081d41b2c487cf2ea4f04d5
Sharafath Ali <sharafathaliofficial@gmail.com>
2026-04-07 00:07:49 +0530

    feat: initialize express application with security, logging, and base route configuration

--- src/app.js
+++ src/app.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(helmet());
 
-app.use(morgan('combined', { stream: logger.stream }));
+app.use(morgan('combined', { stream: { write: (message) => logger.info(message) } }));
 
 app.get('/', (req, res) => {
   logger.info('NexDeal API is running!');
